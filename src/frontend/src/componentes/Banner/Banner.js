aae3799591a690394c7dcf977c16983b694cb6c6
Patrick Quine <patrickquine@hotmail.com>
2024-11-10 14:45:49 -0300

    Feat: add basic React structure

---
+++ src/frontend/src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import "./Banner.css";
+
+function Banner() {
+    return (
+        <header className="banner">
+            <img src="/imagens/elpinho-reto.png" alt="Mascote do projeto ELLP, o Elpinho." />;
+        </header>
+    );
+}
+
+export default Banner;
