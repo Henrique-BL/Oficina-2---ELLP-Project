cd4089f90c462db1b68641ddff6060dc80afa02a
Henrique Lucas <henriquelucas15@outlook.com.br>
2024-12-01 18:23:04 -0300

    feat: added admin

--- src/backend/routers.py
+++ src/backend/routers.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 
 from controllers.VolunteerController import router as volunteer
+from controllers.AdminController import router as admin
+
 api_router = APIRouter()
 api_router.include_router(volunteer, prefix="/volunteers", tags=['volunteers'])
+api_router.include_router(admin, prefix="/admin", tags=['admin'])
