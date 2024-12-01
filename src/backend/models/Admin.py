3ecc78d5862037d4143c67e68b4952a072ccb8ab
Henrique Lucas <henriquelucas15@outlook.com.br>
2024-12-01 18:31:06 -0300

    feat: changed admins

--- src/backend/models/Admin.py
+++ src/backend/models/Admin.py
@@ -1,9 +1,8 @@
 from models.BaseModel import BaseModel
-from sqlalchemy import DateTime, ForeignKey, String, Boolean
+from sqlalchemy import DateTime, String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 from datetime import datetime
-from sqlalchemy.dialects.postgresql import UUID as PG_UUID
-from sqlalchemy import UUID
+
 
 class Admin(BaseModel):
     __tablename__ = "admins"
