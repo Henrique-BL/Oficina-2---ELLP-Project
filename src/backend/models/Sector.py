61d476dd68079f59b5ed9754cd530b01bc28f083
Henrique Lucas <henriquelucas15@outlook.com.br>
2024-12-01 00:37:08 -0300

    feat: added default sectors

--- src/backend/models/Sector.py
+++ src/backend/models/Sector.py
@@ -1,7 +1,8 @@
 from models.BaseModel import BaseModel
-from sqlalchemy import DateTime, Integer, String
+from sqlalchemy import DateTime, Integer, String, event
 from sqlalchemy.orm import Mapped, mapped_column
 from datetime import datetime
+from database.sectors import DEFAULT_SECTORS
 
 class Sector(BaseModel):
     __tablename__ = "sectors"
@@ -10,3 +11,16 @@ class Sector(BaseModel):
     description: Mapped[str] = mapped_column(String(255), nullable=False)
     code_name: Mapped[str] = mapped_column(String(5), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, nullable=False)
+
+# SQLAlchemy event listener that runs after table creation
+@event.listens_for(Sector.__table__, 'after_create')
+def insert_default_sectors(target, connection, **kw):
+    for sector in DEFAULT_SECTORS:
+        code_name = ''.join(word[0].upper() for word in sector['name'].split())[:5]
+        connection.execute(
+            target.insert().values(
+                name=sector['name'],
+                description=sector['description'],
+                code_name=code_name
+            )
+        )
